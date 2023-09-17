9f4a1ba733c5af05d345457fdee822b09fed502a
Yuhang (Tom) Lin <yhlin@iastate.edu>
2023-09-17 16:10:56 -0500

    Add choice of output format for x3p_image_autosize

--- R/x3p_image_autosize.R
+++ R/x3p_image_autosize.R
@@ -2,11 +2,20 @@
 #'
 #' Plot x3p object as an image with auto-adjusted size based on \code{x3ptools::x3p_image}.
 #' @param x3p x3p object
+#' @param ifhtml logical, whether the image will be put on html
 #' @param zoom numeric value indicating the amount of zoom
 #' @param ... other parameter values except size and zoom used in \code{x3ptools::x3p_image}
 #' @importFrom x3ptools x3p_image
+#' @importFrom rgl rglwidget
+#' @importFrom htmltools tagList
 #' @export
 
-x3p_image_autosize <- function(x3p, zoom = 0.6, ...) {
+x3p_image_autosize <- function(x3p, ifhtml = FALSE, zoom = 0.6, ...) {
   x3p_image(x3p, size = dim(x3p$surface.matrix), zoom = zoom, ...)
+
+  if (ifhtml) {
+    rglwidget() %>%
+      tagList() %>%
+      return()
+  }
 }
