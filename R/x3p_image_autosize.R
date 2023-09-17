df024c92db9bd6a8c8248a741b2c3fccf8b3ea78
Yuhang (Tom) Lin <yhlin@iastate.edu>
2023-09-16 19:39:36 -0500

    Add x3p_image_autosize

---
+++ R/x3p_image_autosize.R
@@ -0,0 +1,12 @@
+#' Plot x3p object as an image with auto-adjusted size
+#'
+#' Plot x3p object as an image with auto-adjusted size based on \code{x3ptools::x3p_image}.
+#' @param x3p x3p object
+#' @param zoom numeric value indicating the amount of zoom
+#' @param ... other parameter values used in \code{x3ptools::x3p_image}
+#' @importFrom x3ptools x3p_image
+#' @export
+
+x3p_image_autosize <- function(x3p, zoom = 0.6, ...){
+  x3p_image(x3p, size = dim(x3p$surface.matrix), zoom = zoom)
+}
