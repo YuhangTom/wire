16f03f6028ffe2f486ccf0daf8088c6ac6cedd86
Yuhang (Tom) Lin <yhlin@iastate.edu>
2023-09-22 18:37:58 -0500

    Upload subsampled data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Subsampled wire cuts data
+#'
+#' A list of wire cuts data set by subsampling factor 10 of the original wire cuts x3p.
+#'
+#' @format list
+#' A list of x3p objects
+"x3p_subsamples"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,19 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare `x3p_subsamples` dataset
 
-usethis::use_data(DATASET, overwrite = TRUE)
+x3p_names <- c(
+  "T2AW-LM-R1-B30",
+  "T2AW-LM-R2-B32",
+  "T2CW-LI-R1-B14",
+  "T2CW-LI-R2-B15",
+  "T3CW-LI-R2-B17"
+)
+
+dir <- x3p_names %>%
+  paste0("./data-raw/", .) %>%
+  paste0(".x3p")
+
+x3p_subsamples <- map(dir, x3p_read) %>%
+  map(x3p_average, b = 10) %>%
+  set_names(x3p_names)
+
+usethis::use_data(x3p_subsamples, overwrite = TRUE)
