b6d270d9bfff4b184e279c61dd2e2f56d90fe99c
Yuhang (Tom) Lin <yhlin@iastate.edu>
2023-10-05 18:54:16 -0500

    Faster by computing sd only when ifplot = TRUE

--- R/x3p_insidepoly_df.R
+++ R/x3p_insidepoly_df.R
@@ -25,7 +25,7 @@
 #' x3p <- x3p_subsamples[[1]]
 #'
 #' x3p_insidepoly_df(x3p, mask_col = "#FF0000", concavity = 1.5, b = 10, ifplot = TRUE) %>%
-#' str()
+#'   str()
 #'
 x3p_insidepoly_df <- function(x3p, mask_col = "#FF0000", concavity = 1.5, b = 10,
                               ifplot = FALSE) {
@@ -55,7 +55,7 @@ x3p_insidepoly_df <- function(x3p, mask_col = "#FF0000", concavity = 1.5, b = 10
   x3p_inner <- x3p_extract(x3p, mask_vals = mask_col)
   if (b > 1) {
     # only need to average when the block size is greater than 1
-     x3p_inner <- x3p_inner %>% x3p_average(b = b, na.rm = TRUE)
+    x3p_inner <- x3p_inner %>% x3p_average(b = b, na.rm = TRUE)
   }
 
   resolution <- x3p_inner %>% x3p_get_scale()
@@ -63,42 +63,46 @@ x3p_insidepoly_df <- function(x3p, mask_col = "#FF0000", concavity = 1.5, b = 10
   x3p_inner_df <- x3p_inner %>%
     x3p_to_df() %>%
     mutate(
-      x = round(x/resolution),
-      y = round(y/resolution)
+      x = round(x / resolution),
+      y = round(y / resolution)
     )
 
-
-
-
   x3p_inner_raster <- t(x3p_inner$surface.matrix) %>%
-    raster(xmx = (x3p_inner$header.info$sizeX-1), ymx = (x3p_inner$header.info$sizeY-1))
+    raster(xmx = (x3p_inner$header.info$sizeX - 1), ymx = (x3p_inner$header.info$sizeY - 1))
 
   # x3p_inner_raster %>% as.data.frame(xy=TRUE) %>% ggplot(aes(x=x, y=y, fill=layer)) + geom_raster()
 
   # counts the number of non-missing values:
-  n_neighbor_val_miss <- focal(is.na(x3p_inner_raster), w=matrix(1, ncol=3, nrow=3), fun=sum)
-# n_neighbor_val_miss %>% as.data.frame(xy=TRUE) %>% ggplot(aes(x=x, y=y, fill=factor(layer))) + geom_raster()
+  n_neighbor_val_miss <- focal(is.na(x3p_inner_raster), w = matrix(1, ncol = 3, nrow = 3), fun = sum)
+  # n_neighbor_val_miss %>% as.data.frame(xy=TRUE) %>% ggplot(aes(x=x, y=y, fill=factor(layer))) + geom_raster()
 
-#  neighbors <- 9-n_neighbor_val_miss
-#  n_neighbor_val_mean <- focal(x3p_inner_raster, w=matrix(1, ncol=3, nrow=3), fun=sum, na.rm=TRUE)
-#  n_neighbor_val_mean <- n_neighbor_val_mean/neighbors
-#  n_neighbor_val_var <- focal((x3p_inner_raster-n_neighbor_val_mean)^2, w=matrix(1, ncol=3, nrow=3), fun=sum, na.rm=TRUE)
-# #neighbors[] <- pmax(0,neighbors[]-1)
-#  n_neighbor_val_sd <- sqrt(n_neighbor_val_var/9)
-#  n_neighbor_val_sd %>% as.data.frame(xy=TRUE) %>% ggplot(aes(x=x, y=y, fill=layer)) + geom_raster()
+  #  neighbors <- 9-n_neighbor_val_miss
+  #  n_neighbor_val_mean <- focal(x3p_inner_raster, w=matrix(1, ncol=3, nrow=3), fun=sum, na.rm=TRUE)
+  #  n_neighbor_val_mean <- n_neighbor_val_mean/neighbors
+  #  n_neighbor_val_var <- focal((x3p_inner_raster-n_neighbor_val_mean)^2, w=matrix(1, ncol=3, nrow=3), fun=sum, na.rm=TRUE)
+  # #neighbors[] <- pmax(0,neighbors[]-1)
+  #  n_neighbor_val_sd <- sqrt(n_neighbor_val_var/9)
+  #  n_neighbor_val_sd %>% as.data.frame(xy=TRUE) %>% ggplot(aes(x=x, y=y, fill=layer)) + geom_raster()
 
-# # lines above are faster - but we need to adjust the denominator :(
-  n_neighbor_val_sd <- focal(x3p_inner_raster, w=matrix(1, nc=3, nr=3), fun=sd, na.rm=TRUE)
+  # # lines above are faster - but we need to adjust the denominator :(
+  if (ifplot) {
+    n_neighbor_val_sd <- focal(x3p_inner_raster, w = matrix(1, nc = 3, nr = 3), fun = sd, na.rm = TRUE)
+  }
 
   layer <- NULL
-  n_neighbor_df <- n_neighbor_val_miss %>% as.data.frame(xy=TRUE)  %>%
-    rename(n_neighbor_val_miss=layer) %>%
-    mutate(x = round(x), y = round(y))
-  n_neighbor_sd_df <- n_neighbor_val_sd %>% as.data.frame(xy=TRUE)  %>%
-    rename(sd_not_miss=layer) %>%
+  n_neighbor_df <- n_neighbor_val_miss %>%
+    as.data.frame(xy = TRUE) %>%
+    rename(n_neighbor_val_miss = layer) %>%
     mutate(x = round(x), y = round(y))
 
-#  x3p_inner_raster_adj_df <- n_neighbor_df %>% left_join(n_neighbor_sd_df, by=c("x", "y"))
+  if (ifplot) {
+    n_neighbor_sd_df <- n_neighbor_val_sd %>%
+      as.data.frame(xy = TRUE) %>%
+      rename(sd_not_miss = layer) %>%
+      mutate(x = round(x), y = round(y))
+  }
+
+  #  x3p_inner_raster_adj_df <- n_neighbor_df %>% left_join(n_neighbor_sd_df, by=c("x", "y"))
 
   # ### Compute the adjacent (neighbor) cells with queen's case directions, including self
   # x3p_inner_raster_adj <- adjacent(x3p_inner_raster, cells = 1:ncell(x3p_inner_raster), directions = 8, pairs = TRUE, sorted = TRUE, include = TRUE)
@@ -120,17 +124,26 @@ x3p_insidepoly_df <- function(x3p, mask_col = "#FF0000", concavity = 1.5, b = 10
   #     sd_not_miss = sd(neighbor_val, na.rm = TRUE)
   #   )
 
-  x3p_inner_df <- x3p_inner_df %>%
-    left_join(n_neighbor_df, by=c("x", "y")) %>%
-    left_join(n_neighbor_sd_df, by=c("x", "y")) %>%
-    mutate(
-      n_neighbor_val_miss = factor(n_neighbor_val_miss),
-      x = x * resolution,
-      y = y * resolution
-    )
+  if (ifplot) {
+    x3p_inner_df <- x3p_inner_df %>%
+      left_join(n_neighbor_df, by = c("x", "y")) %>%
+      left_join(n_neighbor_sd_df, by = c("x", "y")) %>%
+      mutate(
+        n_neighbor_val_miss = factor(n_neighbor_val_miss),
+        x = x * resolution,
+        y = y * resolution
+      )
+  } else {
+    x3p_inner_df <- x3p_inner_df %>%
+      left_join(n_neighbor_df, by = c("x", "y")) %>%
+      mutate(
+        n_neighbor_val_miss = factor(n_neighbor_val_miss),
+        x = x * resolution,
+        y = y * resolution
+      )
+  }
 
   if (ifplot) {
-
     (x3p_inner_df %>%
       ggplot(aes(x = x, y = y, fill = value)) +
       geom_raster() +
